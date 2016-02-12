89b841dd30b41715b42797d04bcf341d53835f5b
shumphries <seth.humphries@sdl.usu.edu>
2016-02-11 17:19:09 -0700

    small updates to install files

--- installation/secondaryInsalls.sh
+++ installation/secondaryInsalls.sh
@@ -14,8 +14,8 @@ mkdir "${idir}"
 
 ## sublime text3
 cd ${install_dir}
-wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
-sudo dpkg -i ./sublime-text_*
+wget https://download.sublimetext.com/sublime-text_build-3103_amd64.deb
+sudo dpkg -i ./sublime-text_build-3103_amd64.deb
 wget https://packagecontrol.io/Package%20Control.sublime-package
 sudo cp ./Package*sublime* /opt/sublime_text/Packages/
 
