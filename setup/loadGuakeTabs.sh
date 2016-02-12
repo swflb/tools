b80f67b3601e37ca9b8679a06b8c148069d8fff8
shumphries <seth.humphries@sdl.usu.edu>
2016-02-11 17:19:53 -0700

    create guake tabs on startup

--- setup/loadGuakeTabs.sh
+++ setup/loadGuakeTabs.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 firstDev="/home/seth/dev/mdp"
-secDev="/home/seth/secondMDP"
+secDev="/home/seth/dev/secondMDP"
 BD="build"
-mkdir -p ${firstDev}/${BD}
-mkdir -p ${secDev}/${BD}
-#sleep 5
+sleep 6
 guake --new-tab=1 -r "mbuild" --execute-command="cd ${firstDev}/${BD}"
-#sleep 2
 guake --new-tab=2 -r "mdp" --execute-command="cd ${firstDev}"
-#sleep 2
 guake --new-tab=3 -r "2build" --execute-command="cd ${secDev}/${BD}"
-#sleep 2
 guake --new-tab=4 -r "2mdp" --execute-command="cd ${secDev}"
