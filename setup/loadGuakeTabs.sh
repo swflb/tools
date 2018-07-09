a0fec8a31329d7e9018433c3ce6c1b58f152b306
seth humphries <professorhumphries@gmail.com>
2018-07-09 07:57:01 -0600

    update guake setup script

--- setup/loadGuakeTabs.sh
+++ setup/loadGuakeTabs.sh
@@ -3,8 +3,8 @@ firstDev="/home/shumphries/dev/mdp"
 secDev="/home/shumphries/dev/secondMDP"
 firstBuild="/home/shumphries/build/mdpbuild"
 secBuild="/home/shumphries/build/secondbuild"
-sleep 9
-guake --new-tab=1 -r "mbuild" --execute-command="cd ${firstBuild}"
-guake --new-tab=2 -r "mdp" --execute-command="cd ${firstDev}"
-guake --new-tab=3 -r "2build" --execute-command="cd ${secBuild}"
-guake --new-tab=4 -r "2mdp" --execute-command="cd ${secDev}"
+sleep 2
+guake --new-tab="${firstBuild}" --rename-current-tab="mbuild";
+guake --new-tab="${firstDev}" --rename-current-tab="mdp";
+guake --new-tab="${secBuild}" --rename-current-tab="2build";
+guake --new-tab="${secDev}" --rename-current-tab="2mdp";
